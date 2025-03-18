4ba2f4a63ff31dd1a3700b8a1366a608355cc528
Olivier ROMAN <oromantik@gmail.com>
2025-03-18 15:58:54 +0100

    add  nix shell

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    cmake
+    SDL2
+    gcc
+    nodejs_23
+    libpng
+    python312Packages.pillow
+  ];
+}
